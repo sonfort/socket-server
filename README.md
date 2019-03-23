81296e9255b4f869be483990ff531819c80f9853
sonfort <sonfort16@hotmail.com>
2019-03-23 13:51:35 -0600

    primer commit rest server

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+# Socket-Server
+
+Reconstruir módulos de Node
+```
+npm install
+```
+
+Generar el dist
+```
+tsc -w
+```
+
+Levantar servidor
+```
+nodemon dist/
+ó
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,29 @@
+import Server from "./classes/server";
+import { SERVER_PORT } from "./global/enviroment";
+import router from "./routes/router";
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+
+
+
+const server = new Server();
+
+
+// Body-parser
+server.app.use( bodyParser.urlencoded({ extended: true }) );
+server.app.use( bodyParser.json() );
+
+
+// Cors
+server.app.use( cors({ origin: true, credentials: true }) );
+
+// Rutas de servicios
+server.app.use('/', router);
+
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto : ${ SERVER_PORT } `)
+});
