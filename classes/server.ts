f45c99b7ef07b88be3e7992bbb8973046d425146
sonfort <rocha.sgl@gmail.com>
2019-03-29 16:33:59 -0600

    avances

--- classes/server.ts
+++ classes/server.ts
@@ -1,20 +1,48 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/enviroment';
+import socketIO from 'socket.io';
+import http from 'http';
+import * as sockets from '../sockets/sockets';
 
 export default class Server {
-    
+
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
+    public io: socketIO.Server;
+    private httpserver: http.Server;
 
-    constructor() {
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpserver = new http.Server(this.app);
+        this.io = socketIO(this.httpserver);
+        this.escucharSockets();
     }
 
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
 
-    start( callback: Function ) {
+    private escucharSockets() {
 
-        this.app.listen( this.port, callback );
+        console.log('Escuchando sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+
+            // Mensajes
+            sockets.mensaje(cliente, this.io);
+
+            // Desconectar
+            sockets.desconectar(cliente);
+
+        });
+    }
+
+    start(callback: Function) {
+
+        this.httpserver.listen(this.port);
 
     }
 
--- index.ts
+++ index.ts
@@ -9,21 +9,21 @@ import cors from 'cors';
 
 
 
-const server = new Server();
+const server = Server.instance;
 
 
 // Body-parser
-server.app.use( bodyParser.urlencoded({ extended: true }) );
-server.app.use( bodyParser.json() );
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
 
 
 // Cors
-server.app.use( cors({ origin: true, credentials: true }) );
+server.app.use(cors({ origin: true, credentials: true }));
 
 // Rutas de servicios
 server.app.use('/', router);
 
 
-server.start( () => {
-    console.log(`Servidor corriendo en el puerto : ${ SERVER_PORT } `)
+server.start(() => {
+    console.log(`Servidor corriendo en el puerto : ${SERVER_PORT} `)
 });
